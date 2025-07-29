0d86dfeb042e7ea2217d99564ca3841324c0f4b5
Timothy Madden <timcmadden@gmail.com>
2025-07-29 14:44:34 -0400

    modsquare

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-let xsize = 10;
-
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(600, 400);
   rectMode(CENTER);
 }
 
 function draw() {
   background(235, 218, 143);
-  fill(25, 100, 100);
-  let sinbase = sin(width * 0.5);
-  let grower = map(sinbase, 0, width, 0, 500);
-  rect(width * 0.5, height * 0.5, xsize + grower, xsize + grower);
+  let t = millis() * 0.002;
+  // Size will oscillate between 100 and 200
+  let size = map(sin(t), -1, 1, 100, 200);
+  fill(70, 125, 60);
+  noStroke();
+  rect(width / 2, height / 2, size, size);
 }
