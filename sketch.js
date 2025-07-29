617fc543fbaad7bfa1709c20194ca80d113b0a9a
Timothy Madden <timcmadden@gmail.com>
2025-07-29 08:40:04 -0400

    added sin square

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+let xsize = 10;
+
 function setup() {
   createCanvas(500, 500);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(235, 218, 143);
   fill(25, 100, 100);
-  rect(width * 0.5, height * 0.5, 100, 100);
+  let sinbase = sin(width * 0.5);
+  let grower = map(sinbase, 0, width, 0, 500);
+  rect(width * 0.5, height * 0.5, xsize + grower, xsize + grower);
 }
